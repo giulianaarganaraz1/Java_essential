4d00798d647c515e563e2ab64469857b3c94831c
GiulianaArgañaraz <ibonuzumaki@gmail.com>
2026-03-29 23:30:52 -0300

    agregando mis primeros documentos

---
+++ src/JavaBasico/condicional.java
@@ -0,0 +1,13 @@
+package JavaBasico;
+
+public class condicional {
+    public static void main (String[] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        } else {
+            System.out.println("x NO es 3");
+        }
+        System.out.println("This runs no matter what");
+    }
+}
